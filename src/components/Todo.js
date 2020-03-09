46c937560ffad9c336c3b56efcf895fc95e6cc43
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-03-09 16:17:46 -0600

    Put checkbox in Todo component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,10 +2,12 @@ import React from "react";
 
 export default function Todo(props) {
   return (
-    <div
-      className={props.todo.checked ? "task completed" : "task"}
-      onClick={() => props.toggleComplete(props.todo.id)}
-    >
+    <div className={props.todo.checked ? "task completed" : "task"}>
+      <input
+        type="checkbox"
+        checked={props.todo.checked}
+        onChange={ev => props.toggleComplete(props.todo.id)}
+      />
       {props.todo.description}
     </div>
   );
